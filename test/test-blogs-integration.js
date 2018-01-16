51e8a0e6edaa789eb27054a1377bcf7c982c3ef9
Jeff Lin <fishtailwrinkle@gmail.com>
2018-01-15 17:54:44 -0800

    started creating tests

--- test/test-blogs-integration.js
+++ test/test-blogs-integration.js
@@ -5,4 +5,36 @@ const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
 
+// this makes the expect syntax available throughout this module
+const expect = chai.expect;
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+// used to put randomish documents in db
+function seedBlogData() {
+	console.info('Seeding blog data');
+	const seedData = [];
+
+	for (let i=1; i<=10; i++) {
+		seedData.push(generateBlogData());
+	}
+
+	return BlogPost.insertMany(seedData);
+}
+
+//generateBlogData()
+
+/*
+author: {
+    firstName: String,
+    lastName: String
+  },
+  title: {type: String, required: true},
+  content: {type: String},
+  created: {type: Date, default: Date.now}
+*/
+
 
